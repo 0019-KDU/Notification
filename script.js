1b72bd5fc1dbcb86e09eb0f43507ec395e97cbb5
chirantha <chirantharavishka@gmail.com>
2023-09-22 15:12:20 +0530

    Add JavaScript for Sending Push Notifications
    This commit adds the necessary JavaScript code for sending push notifications using Firebase Cloud Messaging (FCM). Here's an overview of the changes made:
    
    - Integrated Firebase Cloud Messaging (FCM) to send push notifications.
    - Created a form with two input fields: one for the recipient's device registration token and another for the message.
    - Implemented form submission handling to send notifications to the specified device.
    - Added basic error handling for failed notifications.

---
+++ script.js
@@ -0,0 +1,41 @@
+const serverKey = "YOUR_SERVER_KEY";
+const fcmEndpoint = "https://fcm.googleapis.com/fcm/send";
+
+document
+  .getElementById("message-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting traditionally
+
+    const recipient = document.getElementById("recipient").value;
+    const messageText = document.getElementById("message").value;
+
+    const message = {
+      to: recipient,
+      notification: {
+        title: "Notification Title",
+        body: messageText,
+      },
+    };
+
+    fetch(fcmEndpoint, {
+      method: "POST",
+      headers: {
+        Authorization: `key=${serverKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to send notification.");
+        }
+        console.log("Notification sent successfully.");
+      })
+      .catch((error) => {
+        console.error("Error sending notification:", error);
+      });
+
+    // Clear the form fields
+    document.getElementById("recipient").value = "";
+    document.getElementById("message").value = "";
+  });
