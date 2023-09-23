57dde12e6e76e34f3d2476a01316392f1c1a31df
chirantha <chirantharavishka@gmail.com>
2023-09-23 17:27:25 +0530

    change all file
    added new function for js file

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const serverKey = "AAAAmBeVi4I:APA91bFHzBZI544BqTpZYgU-Fn8wztYJvgSJXWlA0R4zpOy4gZ22ndhZsyltBam-1zZu_VpyjdMtZnQKD0AcOlNL0-FCNdWQaCFF2hITkTSwppm5mP5207Ehv_vERNr9dfIf_PHfgI0v";
+const serverKey =
+  "AAAAmBeVi4I:APA91bFHzBZI544BqTpZYgU-Fn8wztYJvgSJXWlA0R4zpOy4gZ22ndhZsyltBam-1zZu_VpyjdMtZnQKD0AcOlNL0-FCNdWQaCFF2hITkTSwppm5mP5207Ehv_vERNr9dfIf_PHfgI0v";
 const fcmEndpoint = "https://fcm.googleapis.com/fcm/send";
 
 document
@@ -6,36 +7,59 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the form from submitting traditionally
 
-    const recipient = document.getElementById("recipient").value;
     const messageText = document.getElementById("message").value;
+    const titleText = document.getElementById("title").value;
 
-    const message = {
-      to: recipient,
-      notification: {
-        title: "Notification Title",
-        body: messageText,
-      },
-    };
+    // Fetch tokens from Firestore and append them to registration_ids
+    fetchTokensFromFirestore().then((tokens) => {
+      const message = {
+        registration_ids: tokens, // Use 'registration_ids' for multiple tokens
+        notification: {
+          title: titleText,
+          body: messageText,
+        },
+      };
 
-    fetch(fcmEndpoint, {
-      method: "POST",
-      headers: {
-        Authorization: `key=${serverKey}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(message),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to send notification.");
-        }
-        console.log("Notification sent successfully.");
+      fetch(fcmEndpoint, {
+        method: "POST",
+        headers: {
+          Authorization: `key=${serverKey}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(message),
       })
-      .catch((error) => {
-        console.error("Error sending notification:", error);
-      });
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to send notification.");
+          }
+          console.log("Notification sent successfully.");
+        })
+        .catch((error) => {
+          console.error("Error sending notification:", error);
+        });
+    });
 
-    // Clear the form fields
-    document.getElementById("recipient").value = "";
+    // Clear the form field
     document.getElementById("message").value = "";
   });
+
+async function fetchTokensFromFirestore() {
+  const tokens = [];
+
+  // Initialize Firebase with your config
+
+  const db = firebase.firestore();
+  const tokensCollection = db.collection("tokens");
+
+  // Assuming you have a document in the 'tokens' collection that contains an array of tokens
+  const tokensDocument = await tokensCollection.doc("your_document_id").get();
+
+  if (tokensDocument.exists) {
+    const tokenData = tokensDocument.data();
+    if (tokenData && tokenData.tokens) {
+      tokens.push(...tokenData.tokens);
+    }
+  }
+
+  return tokens;
+}
