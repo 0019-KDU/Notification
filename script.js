9bb9bdd13c4937894e11581937c5b976f8a7f21b
chirantha <chirantharavishka@gmail.com>
2023-09-23 18:10:53 +0530

    Edit js file
    edit js file

--- script.js
+++ script.js
@@ -1,73 +1,65 @@
-const serverKey =
-  "AAAAmBeVi4I:APA91bFHzBZI544BqTpZYgU-Fn8wztYJvgSJXWlA0R4zpOy4gZ22ndhZsyltBam-1zZu_VpyjdMtZnQKD0AcOlNL0-FCNdWQaCFF2hITkTSwppm5mP5207Ehv_vERNr9dfIf_PHfgI0v";
-const fcmEndpoint = "https://fcm.googleapis.com/fcm/send";
+// Import Firebase modules as ES modules
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhNHaL-JcSUqHrBZd28syW_qLSHFK-edY",
+  authDomain: "lego-3fe7c.firebaseapp.com",
+  projectId: "lego-3fe7c",
+  storageBucket: "lego-3fe7c.appspot.com",
+  messagingSenderId: "653230705538",
+  appId: "1:653230705538:web:1c380451712cb5caa2dd9b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Function to fetch tokens from Firestore
+async function fetchTokensFromFirestore() {
+  const tokens = [];
+
+  // Use the Firebase JavaScript SDK to fetch tokens from Firestore
+  const db = firebase.firestore();
+  const tokensCollection = db.collection("tokens");
+
+  // Assuming you have a document in the 'tokens' collection that contains an array of tokens
+  const tokensDocument = await tokensCollection.get();
+
+  if (!tokensDocument.empty) {
+    tokensDocument.forEach((doc) => {
+      tokens.push(doc.id);
+    });
+  }
+
+  return tokens;
+}
+
+// Rest of your code remains the same
+// ...
+
+// Initialize the form and handle submissions
 document
   .getElementById("message-form")
   .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting traditionally
+    event.preventDefault();
 
     const messageText = document.getElementById("message").value;
     const titleText = document.getElementById("title").value;
 
-    // Fetch tokens from Firestore and append them to registration_ids
     fetchTokensFromFirestore().then((tokens) => {
       const message = {
-        registration_ids: tokens, // Use 'registration_ids' for multiple tokens
+        registration_ids: tokens,
         notification: {
           title: titleText,
           body: messageText,
         },
       };
 
-      fetch(fcmEndpoint, {
-        method: "POST",
-        headers: {
-          Authorization: `key=${serverKey}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(message),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Failed to send notification.");
-          }
-          console.log("Notification sent successfully.");
-        })
-        .catch((error) => {
-          console.error("Error sending notification:", error);
-        });
+      // The rest of your code for sending notifications
+      // ...
+
+      // Clear the form field
+      document.getElementById("message").value = "";
     });
-
-    // Clear the form field
-    document.getElementById("message").value = "";
   });
-
-async function fetchTokensFromFirestore() {
-  const tokens = [];
-
-  // Initialize Firebase with your config
-  const firebaseConfig = {
-    apiKey: "AIzaSyAhNHaL-JcSUqHrBZd28syW_qLSHFK-edY",
-    authDomain: "lego-3fe7c.firebaseapp.com",
-    projectId: "lego-3fe7c",
-    storageBucket: "lego-3fe7c.appspot.com",
-    messagingSenderId: "653230705538",
-    appId: "1:653230705538:web:1c380451712cb5caa2dd9b",
-  };
-  // Initialize Firebase
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-
-  const db = firebase.firestore();
-  const tokensCollection = db.collection("tokens");
-  const tokenData = [];
-  // Assuming you have a document in the 'tokens' collection that contains an array of tokens
-  if (tokensDocument.exists) {
-    for (const items in tokensCollection.docs) {
-      tokenData.add(items.id);
-    }
-  }
-  return tokenData;
-}
